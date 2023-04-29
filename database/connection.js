63974b5e5a3a398f47db0f591c0f885b2d02fbb1
Harshit <harshitmitta070@gmail.com>
2023-04-29 23:56:44 +0530

    Temp

--- database/connection.js
+++ database/connection.js
@@ -8,7 +8,12 @@ async function connect() {
   }
 
   console.log("Connecting with mongodb");
-
+  setTimeout(() => {
+    if (connection.isConnected) {
+      console.log("Connected to database");
+    }
+    console.log("Not Connected");
+  }, 5000);
   try {
     const db = await mongoose.connect(process.env.DATABASE_URL);
     connection.isConnected = db.connections[0].readyState;
