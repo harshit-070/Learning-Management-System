7c34b0e910ecd51d2ad266bb3d4a4f46b967d3e5
Harshit <harshitmitta070@gmail.com>
2023-05-02 18:13:41 +0530

    .

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,7 @@
-const baseUrl = process.env.BASE_URL
-  ? process.env.BASE_URL
-  : "http://localhost:3000";
+// const baseUrl = process.env.BASE_URL
+//   ? process.env.BASE_URL
+//   : "http://localhost:3000";
+
+const baseUrl = "https://lms-bice-sigma.vercel.app";
 
 export default baseUrl;
